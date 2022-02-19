5ce2ef16ab92169c4d02257a63a477b948e5428f
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-19 21:58:15 +0700

    copy lagi

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-    'dsn'          => '',
+    'dsn'          => 'mssql-69555-0.cloudclusters.net,19048',
     'hostname'     => 'mssql-69555-0.cloudclusters.net',
 	'port'		   =>  19048,
     'username'     => 'admin',
