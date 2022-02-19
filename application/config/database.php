2ff9c090f7385b7b1c1f457426831bff04659c60
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-20 01:05:23 +0700

    test aja

--- application/config/database.php
+++ application/config/database.php
@@ -83,7 +83,7 @@ $db['default'] = array(
     'dbdriver'     => 'sqlsrv',
     'dbprefix'     => '',
     'pconnect'     => FALSE,
-    'db_debug'     => TRUE,
+    'db_debug'     => FALSE,
     'cache_on'     => FALSE,
     'cachedir'     => '',
     'char_set'     => 'utf8',
@@ -93,5 +93,5 @@ $db['default'] = array(
     'compress'     => FALSE,
     'stricton'     => FALSE,
     'failover'     => array(),
-    'save_queries' => FALSE
+    'save_queries' => TRUE
 ); 
