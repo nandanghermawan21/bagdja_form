437b4ef87664e76734090468fbab967ba573e38c
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-19 21:41:17 +0700

    changa lagi

--- application/config/database.php
+++ application/config/database.php
@@ -73,26 +73,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'mssql-69555-0.cloudclusters.net',
-	'port' => '19048',
-	'username' => 'admin',
-	'password' => '@H3rm4w4n',
-	'database' => 'bagdja_form',
-	'dbdriver' => 'sqlsrv',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => true,
-	'cache_on' => TRUE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => false,
-	'autoinit' => TRUE
-);
+$db['mssql'] = array(
+    'dsn'          => '',
+    'hostname'     => 'mssql-69555-0.cloudclusters.net',
+    'username'     => 'admin',
+    'password'     => '@H3rm4w4n',
+    'database'     => 'bagdja_form',
+    'dbdriver'     => 'sqlsrv',
+    'dbprefix'     => '',
+    'pconnect'     => FALSE,
+    'db_debug'     => (ENVIRONMENT !== 'production'),
+    'cache_on'     => FALSE,
+    'cachedir'     => '',
+    'char_set'     => 'utf8',
+    'dbcollat'     => 'utf8_general_ci',
+    'swap_pre'     => '',
+    'encrypt'      => false,
+    'compress'     => FALSE,
+    'stricton'     => FALSE,
+    'failover'     => array(),
+    'save_queries' => FALSE
+); 
